0e3c933bac3d3e5198ab046ba0260a5bf2192b66
flortoledo85 <florenciaetoledo@gmail.com>
2025-12-06 19:59:21 -0300

    Agreagado funciones para agregar producto al carrito, vaciar carrito y cargar datos desde JSON

--- script.js
+++ script.js
@@ -1,63 +1,28 @@
-
-// CARRITO
-
-const carrito = {
-    productos: [],
-    agregar: function (producto) {
-        this.productos.push(producto);
-        console.log(`"${producto.name}" fue agregado al carrito`);
-    },
-    obtenerTotal: function () {
-        const total = calcularTotal(this.productos);
-        console.log("El monto total es : $" + total);
-        return total;
-    }
-}
-
 // FUNCIONES DE MI PROGRAMA
 
+let listaCarrito = [];
 
-function calcularTotal(productos) {
-    let total = 0;
-    for (let i = 0; i < productos.lenght; i++) {
-        total += productos[i].amount;
-    }
-    return total;
-}
+const productos = [];
+//JSON
+/**
+ * Funcion asincronica para traer los objetos de JSON
+ * @returns {Promise<Array<Object>>}
+ */
 
-
-function generarProductos() {
-    const productos = [
-        {
-            id: 1,
-            name: "PinkLove",
-            description: "Armazon de acetato transparante. Color: Rosa. Medidas: ancho total 139.7 mm.Largo patilla: 145 mm. Alto lente: 44 mm.Ancho: 53 mm.DIP: 61 a 85 mm.",
-            pathimg: "./pictures/pink_love.jpg",
-            amount: 90000
-        },
-        {
-            id: 2,
-            name: "Cristal",
-            description: "Armazon de acetato transparante. Color: transparante. Medidas: ancho total 142.5 mm. Largo patilla: 147 mm. Alto lente: 45 mm. Ancho: 56 mm. DIP: 61 a 85 mm.",
-            pathimg: "./pictures/sunglasses.jpg",
-            amount: 120000
-        },
-        {
-            id: 3,
-            name: "Sol Bordo",
-            description: "Anteojos de Sol. Color: Bordo. Medidas: ancho total 139.7 mm. Largo patilla: 145 mm. Alto lente: 44 mm. Ancho: 53 mm. DIP: 61 a 85 mm.",
-            pathimg: "./pictures/cristal.jpg",
-            amount: 198000
-        },
-        {
-            id: 4,
-            name: "Jaspeado",
-            description: "Armazon marron jaspeado. Color: Bordo. Medidas: ancho total 141.5 mm. Largo patilla: 152 mm. Alto lente: 46 mm. Ancho: 53 mm. DIP: 61 a 85 mm.",
-            pathimg: "./pictures/jaspeado.jpg",
-            amount: 118000
+async function cargarProductosApi() {
+    try {
+        const respuesta = await fetch("./productos.json");
+        if(!respuesta.ok) {
+            throw new Error(`Error al ;obtener los datos ${respuesta.status} - ${respuesta.statusText}`);
         }
-    ];
-    return productos;
+        const productosArray = await respuesta.json();
+        return productosArray;
+    }
+    catch (error) {
+        console.error("Fallo en la carga de datos: ", error);
+        const listaUL = document.querySelector("#productos .catalogo");
+        listaUL.innerHTML = `<li id="mensaje-error"> Error al cargar el catalogo.</li>`
+    }
 }
 
 function crearProducto(idProd, nameProd, descriptionProd, pathProducto, amountProd) {
@@ -87,29 +52,53 @@ function mostrarCatalogo2(catalogo) {
     };
 }
 
+function calcularTotal(productos) {
+    let total = 0;
+    for (let i = 0; i < productos.lenght; i++) {
+        total += productos[i].amount;
+    }
+    return total;
+}
+
+function buscarEnLista(catalogo, idBuscado) {
+    for (const { id } of catalogo) {
+        if (id === idBuscado) {
+            return id
+        }
+    }
+    return -1;
+}
+
+function buscarProductoID(catalogo, idBuscado) {
+    for (const item of catalogo) {
+        if (item.id === idBuscado) {
+            const { description, ...resto } = item;
+            return resto;
+        }
+    }
+    return -1;
+}
+
+
 function agregarProducto(catalogo, nuevoProducto) {
     const catalogoActualizado = [...catalogo, nuevoProducto];
     return catalogoActualizado
 }
 
 function insertarProducto(listaNueva) {
-    const listaProductos = copiaListaProductos(listaNueva);
+    // const listaProductos = copiaListaProductos(listaNueva);
     const contenedorProductos = document.querySelector("#productos .catalogo");
     // console.log(contenedorProductos);
 
     for (let c = 0; c < listaNueva.length; c++) {
         // console.log(c)
         const productoActual = listaNueva[c];
-
         const nuevoElemento = document.createElement("article");
         nuevoElemento.className = "producto";
-        const precio = new Intl.NumberFormat(navigator.language, {
-            style: "currency",
-            currency: "ARS"
-        }).format(productoActual.amount);
+        const precio = productoActual.amount
         nuevoElemento.innerHTML = `
-            <a href=${productoActual.pathimg} target="_blank">
-            <img src=${productoActual.pathimg}></a>
+            <a href="${productoActual.pathimg}" target="_blank">
+            <img src="${productoActual.pathimg}" alt="${productoActual.description}"></a>
             <h3>${productoActual.name}</h3>
             <p>
                 <a class="description_product" href="#." data-descripcion="${productoActual.description}"> 
@@ -118,25 +107,50 @@ function insertarProducto(listaNueva) {
             <div class="contenedor_descripcion">
             </div> 
             <p class= "precio">
-                Precio = ${precio}<br>
+                Precio = ${precio.toLocaleString("es-AR", { style: "currency", currency: "ARS" })}<br>
             </p>
             <button 
                 type="button" 
                 data-id="${productoActual.id}"
                 data-nombre="${productoActual.name}"
-                data-precio="${precio}"
-                >
+                data-precio="${precio}">
                 Agregar al Carrito
             </button>`
         contenedorProductos.appendChild(nuevoElemento);
     }
 }
 
+function insertarProductoHTML(producto) {
+    const listaCarrito = document.querySelector("#carrito .list-group");
+    const liProducto = document.createElement("li");
+    console.log("Producto recibido:", producto);
+    console.log("Tiene amount?", producto.amount);
+    console.log("Tiene precio?", producto.precio);
+    liProducto.textContent = `${producto.name} ${producto.amount.toLocaleString("es-AR", { style: "currency", currency: "ARS" })}`;
+    liProducto.className = "list-group-item";
+    listaCarrito.appendChild(liProducto);
+}
+
+function actualizaContador() {
+    const contenedorNumero = document.querySelector("#carrito .contador_compra");
+    contenedorNumero.textContent = listaCarrito.length;
+}
+
+function guardarCarritoEnStorage(listaCarrito) {
+    const carritoJSON = JSON.stringify(listaCarrito);
+    localStorage.setItem("listaCarrito", carritoJSON);
+}
+
+function vaciarCarritoHTML() {
+    const listaVaciar = document.querySelector("#carrito .list-group");
+    listaVaciar.innerHTML = "";
+}
+
 function mostrarDescripcion(evento) {
     const elementoEvento = evento.target.tagName;
     if (elementoEvento === "A") {
         const elementoClickeado = evento.target;
-        const descripcionProducto = elementoClickeado.dataset.descripcion
+        const descripcionProducto = elementoClickeado.dataset.descripcion;
         // console.log(descripcionProducto)
         const divProducto = elementoClickeado.closest(".producto");
         const divDescrip = divProducto.querySelector(".contenedor_descripcion");
@@ -154,21 +168,70 @@ function mostrarDescripcion(evento) {
     }
 }
 
+
+function agregarAlCarrito(evento) {
+
+    if (evento.target.tagName === "BUTTON") {
+        const idProducto = parseInt(evento.target.dataset.id);
+        const idEncontrado = buscarEnLista(listaCarrito, idProducto);
+
+        if (idEncontrado === -1) {
+            const productoEncontrado = buscarProductoID(listaNueva, idProducto);
+            listaCarrito.push(productoEncontrado);
+            insertarProductoHTML(productoEncontrado);
+            actualizaContador();
+            guardarCarritoEnStorage(listaCarrito);
+        }
+    }
+}
+
+function cargarCarritoStorage() {
+    const carritoJSON = localStorage.getItem("listaCarrito");
+    console.log(carritoJSON);
+    if (carritoJSON) {
+        return JSON.parse(carritoJSON);
+    }
+    else {
+        return [];
+    }
+}
+
+
+function eliminarCarritoEnStorage() {
+    localStorage.removeItem("listaCarrito");
+    listaCarrito = [];
+    actualizaContador();
+    vaciarCarritoHTML();
+}
+
 // EJECUCION DE MI PROGRAMA
 
-const lista = generarProductos();
-const nuevo = crearProducto(5, "Rayban Polarizado", "Anteojos de Sol. RayBan P. Polarizados Color: Negro. Medidas: ancho total 139.7 mm. Largo patilla: 145 mm. Alto lente: 55 mm. DIP 55 a 70 mm.", "./pictures/rayban_p.jpg", 220000,)
-const listaNueva = agregarProducto(lista, nuevo)
+// const lista = generarProductos();
+// const nuevo = crearProducto(5, "Rayban Polarizado", "Anteojos de Sol. RayBan P. Polarizados Color: Negro. Medidas: ancho total 139.7 mm. Largo patilla: 145 mm. Alto lente: 55 mm. DIP 55 a 70 mm.", "./pictures/rayban_p.jpg", 220000.00)
+// const listaNueva = agregarProducto(productos, nuevo)
 
-mostrarCatalogo1(lista);
-console.log("--------------------");
-mostrarCatalogo2(listaNueva);
-console.log("--------------------");
-const listaCopiada = copiaListaProductos(listaNueva);
-console.log(listaCopiada)
-console.log("--------------------");
-insertarProducto(listaCopiada)
+// mostrarCatalogo1(lista);
+// console.log("--------------------");
+// mostrarCatalogo2(listaNueva);
+// console.log("--------------------");
+// const listaCopiada = copiaListaProductos(listaNueva);
+// console.log(listaCopiada)
+// console.log("--------------------");
+insertarProducto(listaNueva)
 
+listaCarrito = cargarCarritoStorage();
+
+if (listaCarrito.length != 0) {
+    for (const producto of listaCarrito) {
+        console.log(producto.amount);
+        insertarProductoHTML(producto);
+    }
+    actualizaContador();
+}
 const contenedorProducto = document.querySelector("#productos .catalogo");
-console.log(contenedorProducto);
+// console.log(contenedorProducto);
 contenedorProducto.addEventListener("click", mostrarDescripcion);
+contenedorProducto.addEventListener("click", agregarAlCarrito);
+
+const vaciarCarrito = document.querySelector("#carrito .vaciarCarrito");
+vaciarCarrito.addEventListener("click", eliminarCarritoEnStorage);
